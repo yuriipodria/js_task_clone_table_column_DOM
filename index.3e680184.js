479c50c2cc51668b75192869e688172aefcf0992
Yurii <urabelivr@gmail.com>
2024-06-16 21:07:13 +0200

    make build

---
+++ index.3e680184.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table").rows,l=!0,r=!1,t=void 0;try{for(var n,o=e[Symbol.iterator]();!(l=(n=o.next()).done);l=!0){var a=n.value;a.cells[a.cells.length-1].insertAdjacentElement("beforebegin",a.cells[1].cloneNode(!0))}}catch(e){r=!0,t=e}finally{try{l||null==o.return||o.return()}finally{if(r)throw t}}
+//# sourceMappingURL=index.3e680184.js.map
